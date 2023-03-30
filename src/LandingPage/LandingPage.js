a5a2fc3ec353b0e866966c1378330bf76ec10651
Akash8377 <121550790+Akash8377@users.noreply.github.com>
2023-03-30 16:23:44 +0530

    Update LandingPage.js

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import img from "../imgs/IMG.jpg";
+import img from "../imgs/Landing_img.webp";
 export default function Landing_page() {
   const routes = [{ key: "Enter", route: "/postview" }];
   return (
     <>
       <div className="landing_main_container">
         <div className="landing_img_container">
-          <img src={img} alt="img" />
+          <img src={img} alt="img"/>
         </div>
         <section className="landing_button_container">
-          <p>10x Team 04</p>
+          <p>Enter Akash Insta</p>
           <button>
             <Link to={routes[0].route}>{routes[0].key}</Link>
           </button>
